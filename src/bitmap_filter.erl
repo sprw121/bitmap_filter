4a565b725e024703d75bc41540874a3e09de26b0
Steven Wright <steven.wright@adroll.com>
2015-05-14 00:58:42 -0700

    Generic init code

--- src/bitmap_filter.erl
+++ src/bitmap_filter.erl
@@ -3,16 +3,18 @@
 -export([init/0, filter/1]).
 -on_load(init/0).
 
-%% NOTE : this is a bad idea
-priv_dir(Mod) ->
-    Ebin = filename:dirname(code:which(Mod)),
-    filename:join(filename:dirname(Ebin), "priv").
-
 init() ->
-    PrivDir = priv_dir(strg),
-    ok = erlang:load_nif(filename:join(PrivDir, "bitmap_filter"), 0).
+    PrivDir = case code:priv_dir(?MODULE) of
+        {error, _} ->
+            EbinDir = filename:dirname(code:which(?MODULE)),
+            AppPath = filename:dirname(EbinDir),
+            filename:join(AppPath, "priv");
+        Path ->
+            Path
+    end,
+    erlang:load_nif(filename:join(PrivDir, "bitmap_filter"), 0).
 
- % Hack - overriden by init, which is called in on_load.
- % I couldn't find another way that the compiler or code load didn't complain about.
- filter(DefaultArgs) ->
+% Hack - overriden by init, which is called in on_load.
+% I couldn't find another way that the compiler or code load didn't complain about.
+filter(DefaultArgs) ->
     DefaultArgs.
